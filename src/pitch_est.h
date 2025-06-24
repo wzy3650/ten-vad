4d157d9da19020805b6aa40211996fb2b3cd8e67
wangzeyuan <wangzeyuan@shengwang.cn>
2025-06-24 10:00:32 +0800

    onnx model open source

--- src/pitch_est.h
+++ src/pitch_est.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) [2025] [Agora]. All rights reserved.
+ * 
+ * Original LPCNet code copyright:
+ *   Copyright (c) Mozilla Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ * 
+ * Full license text available in the root directory LICENSE file.
+ */
+
 #ifndef __PITCH_EST_H__
 #define __PITCH_EST_H__
 
