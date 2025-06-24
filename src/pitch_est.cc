925dd1a6c135cb30e835a0b84a4176691d0bebcb
Ziyi Lin <ziyi.lin233@gmail.com>
2025-06-24 11:30:55 +0000

    Update license terms and README.md

--- src/pitch_est.cc
+++ src/pitch_est.cc
@@ -1,27 +1,76 @@
-/*
- * Copyright © 2025 Agora. All rights reserved.
- *
- * This file contains modified code derived from LPCNet (https://github.com/mozilla/LPCNet),
- * specifically from the following functions:
- *   - compute_frame_features() in lpcnet_enc.cc
- *   - process_superframe() in lpcnet_enc.cc
- *
- * Original LPCNet code copyright:
- *   Copyright © Mozilla Foundation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice, 
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of the copyright holder nor the names of its contributors
- *      may be used to endorse or promote products derived from this software without
- *      specific prior written permission.
- *
- * Full license text available in the root directory LICENSE file.
- */
+//
+// Copyright © 2025 Agora
+// This file is part of TEN Framework, an open source project.
+// Licensed under the Apache License, Version 2.0, with certain conditions.
+//
+// This file contains modified code derived from LPCNet (https://github.com/mozilla/LPCNet),
+// specifically from the following functions:
+//   - compute_frame_features() in lpcnet_enc.c
+//   - process_superframe() in lpcnet_enc.c
+//
+// Original lpcnet_enc.c code LICENSE Text, licensed under the BSD-2-Clause License:
+//   Copyright (c) 2017-2019 Mozilla
+//
+//   Redistribution and use in source and binary forms, with or without modification,
+//   are permitted provided that the following conditions are met:
+//
+//   - Redistributions of source code must retain the above copyright notice, 
+//     this list of conditions and the following disclaimer.
+//
+//   - Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//
+// Original LPCNet LICENSE Text, licensed under the BSD-3-Clause License:
+//   Copyright (c) 2017-2018, Mozilla
+//   Copyright (c) 2007-2017, Jean-Marc Valin
+//   Copyright (c) 2005-2017, Xiph.Org Foundation
+//   Copyright (c) 2003-2004, Mark Borgerding
+//
+//   Redistribution and use in source and binary forms, with or without
+//   modification, are permitted provided that the following conditions
+//   are met:
+//
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  
+//   - Neither the name of the Xiph.Org Foundation nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION
+//  OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//   
+
+
+// Refer to the "LICENSE" file in the root directory for more information.
+//
 
 #include <assert.h>
 #include <math.h>
--- src/pitch_est.h
+++ src/pitch_est.h
@@ -1,22 +1,10 @@
-/*
- * Copyright © 2025 Agora. All rights reserved.
- *
- * Original LPCNet code copyright:
- *   Copyright © Mozilla Foundation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice, 
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of the copyright holder nor the names of its contributors
- *      may be used to endorse or promote products derived from this software without
- *      specific prior written permission.
- *
- * Full license text available in the root directory LICENSE file.
- */
+ //
+ // Copyright © 2025 Agora
+ // This file is part of TEN Framework, an open source project.
+ // Licensed under the Apache License, Version 2.0, with certain conditions.
+ //
+ // Refer to the "LICENSE" file in the root directory for more information.
+ //
 
 #ifndef __PITCH_EST_H__
 #define __PITCH_EST_H__
